7badbf10d58d36666aa4e1d61906f1ca50ee3dab
shuvra <shuvrahasan2004@gmail.com>
2025-12-31 18:34:11 +0600

    first commit

--- ticketapp/src/store/authSlice.jsx
+++ ticketapp/src/store/authSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://ticketapp-od6i.onrender.com/api';
 
 export const login = createAsyncThunk('auth/login', async (userData, thunkAPI) => {
     try {
